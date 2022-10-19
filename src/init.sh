06af348196b3aed1c37a37496642c223b0860a75
BETAIDK <idankob@gmail.com>
2022-10-19 23:09:01 +0300

    adding color to the shell

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,6 @@
 init() {
     source $HOME/.config/bash/commands/init.sh
-    source $HOME/.config/bash/profiles/uno.sh
+    PS1="\[$(tput bold)\]\[$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 5)\]\W\[$(tput setaf 1)\]]\[$(tput setaf 7)\]\\$ \[$(tput sgr0)\]"
 }
 
 init
